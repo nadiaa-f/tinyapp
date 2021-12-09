8710359f583e174eb7881b3524e0256d1ef0e18d
Nadia Fatah <nadi.fatah@gmail.com>
2021-12-09 16:04:51 +0000

    created a global object called users used to store & access users and created a registration handler

--- express_server.js
+++ express_server.js
@@ -12,6 +12,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 function generateRandomString() {
   return Math.random().toString(36).substring(2, 8);
 };
@@ -56,6 +69,13 @@ app.get("/u/:shortURL", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {  display: req.cookies.username, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/register", (req, res) => {
+  let userID = generateRandomString();
+  users[userID] = {id: userID, email: req.body.email, password: req.body.password};
+  console.log(users);
+  res.redirect("urls");
 });
 
 app.post("/urls", (req, res) => {
