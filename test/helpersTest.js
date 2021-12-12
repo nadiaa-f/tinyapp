66db953a0207600b8a69eaaa441275f086cb334b
Nadia Fatah <nadi.fatah@gmail.com>
2021-12-12 21:39:16 +0000

    installed mocha and chai, updated package.json scripts, created test folder, and helperTest.js with tests all passing

---
+++ test/helpersTest.js
@@ -0,0 +1,31 @@
+const { assert } = require('chai');
+
+const { findUserViaEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('findUserViaEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = findUserViaEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.deepEqual(user.id, expectedUserID);
+  });
+
+  it('should return undefined if a users email does not exist', function() {
+    const user = findUserViaEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.equal(findUserViaEmail(testUsers, 'user3@example.com'), undefined);
+  });
+
+});
